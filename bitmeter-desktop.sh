bbd9869034c3cd4e23d6053e7662e3b84dab856f
Tim Buckland <tim.buckland@gmail.com>
2025-01-18 13:46:54 +0000

    fix: fix script

--- bitmeter-desktop.sh
+++ bitmeter-desktop.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-conda init
+# Initialize conda for the current shell session
+. /home/test/miniconda3/etc/profile.d/conda.sh
 conda activate bitmeter
 cd /usr/local/bin/bitmeter-desktop
 python main.py
