57fb150980f90037e488540290c5fedb65df7f5c
Tim Buckland <tim.buckland@gmail.com>
2025-01-18 14:13:33 +0000

    fix: remove hardcode

--- bitmeter-desktop.sh
+++ bitmeter-desktop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Initialize conda for the current shell session
-. /home/test/miniconda3/etc/profile.d/conda.sh
+. ~/miniconda3/etc/profile.d/conda.sh
 conda activate bitmeter
 cd /usr/local/bin/bitmeter-desktop
 python main.py
